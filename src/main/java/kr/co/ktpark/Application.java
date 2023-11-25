304080db5e338a31f0940616ecafba2969302a23
ktpark <kma7456@gmail.com>
2023-11-25 23:53:33 +0900

    외부 프로젝트에서 생성한 Holoman 사용   - pom.xml 에 의존성 추가 (ktpark)

--- src/main/java/kr/co/ktpark/Application.java
+++ src/main/java/kr/co/ktpark/Application.java
@@ -3,8 +3,9 @@ package kr.co.ktpark;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.Arrays;
-
+// @SpringBootConfiguration // 그냥 @Configuration 과 동일 (자신을 빈으로 등록)
+// @ComponentScan // 첫번째로 Bean 등록
+// @EnableAutoConfiguration // @ComponentScan Bean 등록 후 두번째로 다시 조회하여 Bean 등록 (이 단계가 없어도 됨)
 @SpringBootApplication
 public class Application {
 
@@ -16,8 +17,11 @@ public class Application {
         --> SpringApplication.run(Application.class, args);
      */
     public static void main(String[] args) {
-        Arrays.stream(args).forEach(System.out::println);
-        SpringApplication.run(Application.class, args);
+
+        SpringApplication springApplication = new SpringApplication(Application.class);
+        // springApplication.setWebApplicationType(WebApplicationType.NONE); // AutoConfiguration 없이 WebApplication 으로 시작되지 않도록 설정
+        springApplication.run(args);
+        // SpringApplication.run(Application.class, args);
     }
 
 }
